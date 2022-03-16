c4607110f8c1e64d803931dbe5c2ebe2b4a2b2fd
kzoeps <kzoepa@gmail.com>
2022-03-16 20:49:26 +0600

    feat: add prettier and format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,43 @@
-import type {NextPage} from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import ototoclub from '../public/images/ototoclub.png';
-import styles from '../styles/Home.module.css';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import ototoclub from "../public/images/ototoclub.png";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Ototo Club</title>
-				<meta name="description"
-					  content="Ototo Club brings to you LK12. An NFT Project around the 12 Zodiac Signs."/>
-				<link rel="icon" href="/favicon.ico"/>
-			</Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Ototo Club</title>
+        <meta
+          name="description"
+          content="Ototo Club brings to you LK12. An NFT Project around the 12 Zodiac Signs."
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
+      </Head>
 
-			<main className={styles.main}>
-				<Image src={ototoclub} alt="Ototo Club"/>
-				<h1 className={styles.title}>
-					Coming Soon
-				</h1>
-			</main>
+      <main className={styles.main}>
+        <Image
+          src={ototoclub}
+          alt="Ototo Club"
+        />
+        <h1 className={styles.title}>Coming Soon</h1>
+      </main>
 
-			<footer className={styles.footer}>
-				<a
-					href="https://www.twitter.com/ototoclub"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Ototo Club © {new Date().getFullYear()}
-				</a>
+      <footer className={styles.footer}>
+        <a
+          href="https://www.twitter.com/ototoclub"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ototo Club © {new Date().getFullYear()}
+        </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
